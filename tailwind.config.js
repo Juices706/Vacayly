cd1c29063b4573c2509f46ddb7595aa3facdb10b
Juices706 <Buhlekzy@gmail.com>
2025-08-06 14:39:18 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vacayGreen: "#00b894"
+      }
+    },
+  },
+  plugins: [],
+}
